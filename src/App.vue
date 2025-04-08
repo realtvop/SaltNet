9d323a9ea7da7c88665ccfc22431f2e069dcdc72
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-04-08 22:38:45 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { getDivingFishData } from "./divingfish";
+import { ref } from 'vue';
+
+// window.g = getDivingFishData;
+const fishData = ref('');
+getDivingFishData("realtvop").then(data => fishData.value = JSON.stringify(data));
+</script>
+
+<template>
+  {{ fishData }}
+</template>
+
+<style scoped>
+</style>
---
+++ src/divingfish/maiTypes.ts
@@ -0,0 +1,19 @@
+export enum FCStatus {
+    None = '',
+    FullCombo = 'fc',
+    FullComboPlus = 'fcp',
+    AllPerfect = 'ap',
+    AllPerfectPlus = 'app',
+}
+
+export enum FSStatus {
+    None = '',
+    FullSync = 'fs',
+    FullSyncPlus = 'fsp',
+    FullSyncDX = 'fsdx',
+    FullSyncDXPlus = 'fspdx',
+}
+
+// export enum rate {
+
+// }
