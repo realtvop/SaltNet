bb1a3564613b501ba1c7c663ed2830fb2c0cc17d
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-04-09 12:47:43 +0800

    refactor: simplify text rendering in ratingPlate component using v-for

--- src/components/ratingPlate.vue
+++ src/components/ratingPlate.vue
@@ -5,29 +5,15 @@ import plates from "../assets/plates";
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" width="100px" height="20px">
       <image :href="plates['10']" x="0" y="0" height="1.2em"/>
-      {{ $attrs.ra.split('').map((char, i) => `
         <text
-          x="${4.9 + (5 - $attrs.ra.length + i) * 0.8}em"
+          v-for="i in 5"
+          :key="i"
+          :x="`${4.9 + (i - 1) * 0.8}em`"
           y="1.45em"
           font-family="Monaco, 'JetBrains Mono', Monospaced, monospace"
           font-size="0.6em"
           fill="#FCD41B">
-          ${char}
-        </text>`).join('') }}
-        <text x="4.9em" y="1.45em" font-family="Monaco, 'JetBrains Mono', Monospaced, monospace" font-size="0.6em" fill="#FCD41B">
-          {{ $attrs.ra[$attrs.ra.length - 5] ?? "" }}
-        </text>
-        <text x="5.7em" y="1.45em" font-family="Monaco, 'JetBrains Mono', Monospaced, monospace" font-size="0.6em" fill="#FCD41B">
-          {{ $attrs.ra[$attrs.ra.length - 4] ?? "" }}
-        </text>
-        <text x="6.5em" y="1.45em" font-family="Monaco, 'JetBrains Mono', Monospaced, monospace" font-size="0.6em" fill="#FCD41B">
-          {{ $attrs.ra[$attrs.ra.length - 3] ?? "" }}
-        </text>
-        <text x="7.3em" y="1.45em" font-family="Monaco, 'JetBrains Mono', Monospaced, monospace" font-size="0.6em" fill="#FCD41B">
-          {{ $attrs.ra[$attrs.ra.length - 2] ?? "" }}
-        </text>
-        <text x="8.1em" y="1.45em" font-family="Monaco, 'JetBrains Mono', Monospaced, monospace" font-size="0.6em" fill="#FCD41B">
-          {{ $attrs.ra[$attrs.ra.length - 1] ?? "" }}
+          {{ $attrs.ra[$attrs.ra.length - (6 - i)] ?? "" }}
         </text>
     </svg>
 </template>
