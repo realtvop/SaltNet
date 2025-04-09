6185afce22ec2b2c75a34a44bfafbf0b2e523e60
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-04-09 12:46:39 +0800

    refactor: remove unused rating component and add rating plate component with SVG generation

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,22 @@
 import { getDivingFishData } from "./divingfish";
 import { ref } from 'vue';
 
+import RatingPlate from "./components/ratingPlate.vue";
+
+// import { generateRatingSvg } from "./utils/genPlate";
+
+
 // window.g = getDivingFishData;
 const fishData = ref('');
-getDivingFishData("realtvop").then(data => fishData.value = JSON.stringify(data));
+getDivingFishData("realtvop").then(data => fishData.value = data);
+
+const ra = "14555";
+// const aaa = generateRatingSvg(ra, plates["10"]);
 </script>
 
 <template>
-  {{ fishData }}
+  <!-- {{ fishData.rating }} -->
+    <RatingPlate :ra="ra" />
 </template>
 
 <style scoped>
---
+++ src/components/ratingPlate.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import plates from "../assets/plates";
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="100px" height="20px">
+      <image :href="plates['10']" x="0" y="0" height="1.2em"/>
+      {{ $attrs.ra.split('').map((char, i) => `
+        <text
+          x="${4.9 + (5 - $attrs.ra.length + i) * 0.8}em"
+          y="1.45em"
+          font-family="Monaco, 'JetBrains Mono', Monospaced, monospace"
+          font-size="0.6em"
+          fill="#FCD41B">
+          ${char}
+        </text>`).join('') }}
+        <text x="4.9em" y="1.45em" font-family="Monaco, 'JetBrains Mono', Monospaced, monospace" font-size="0.6em" fill="#FCD41B">
+          {{ $attrs.ra[$attrs.ra.length - 5] ?? "" }}
+        </text>
+        <text x="5.7em" y="1.45em" font-family="Monaco, 'JetBrains Mono', Monospaced, monospace" font-size="0.6em" fill="#FCD41B">
+          {{ $attrs.ra[$attrs.ra.length - 4] ?? "" }}
+        </text>
+        <text x="6.5em" y="1.45em" font-family="Monaco, 'JetBrains Mono', Monospaced, monospace" font-size="0.6em" fill="#FCD41B">
+          {{ $attrs.ra[$attrs.ra.length - 3] ?? "" }}
+        </text>
+        <text x="7.3em" y="1.45em" font-family="Monaco, 'JetBrains Mono', Monospaced, monospace" font-size="0.6em" fill="#FCD41B">
+          {{ $attrs.ra[$attrs.ra.length - 2] ?? "" }}
+        </text>
+        <text x="8.1em" y="1.45em" font-family="Monaco, 'JetBrains Mono', Monospaced, monospace" font-size="0.6em" fill="#FCD41B">
+          {{ $attrs.ra[$attrs.ra.length - 1] ?? "" }}
+        </text>
+    </svg>
+</template>
+
+<style scoped>
+</style>
