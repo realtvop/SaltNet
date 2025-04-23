c8cd67bc424d32ac7f03ec1f8fae78f8dbd05923
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-04-23 17:48:03 +0800

    feat: 用户列表展示

--- src/pages/users.vue
+++ src/pages/users.vue
@@ -1,24 +1,66 @@
-<template>
-    <div style="height: 10px;"></div>
-    <mdui-card variant="filled">
-        <div class="user-name">
-            <h2 class="primary-name">realtvop</h2>
-            <div class="user-badges">
-                <mdui-chip icon="videogame_asset" elevated disabled>未绑定水鱼</mdui-chip>
-                <mdui-chip icon="local_laundry_service" elevated>GUEST</mdui-chip>
-            </div>
-        </div>
-        <div class="user-actions">
-            <mdui-button end-icon="arrow_forward">详情</mdui-button>
-            <!-- <mdui-button end-icon="settings">设置</mdui-button> -->
-        </div>
-    </mdui-card>
-</template>
-
 <script setup>
+const users = [
+    {
+        divingFish: {
+            name: "realtvop",
+            importToken: "sth",
+        },
+        inGame: {
+            name: "realtvop",
+            id: 10000000,
+        },
+    },
+    {
+        divingFish: {
+            name: null,
+            importToken: null,
+        },
+        inGame: {
+            name: "OnlyInGame",
+            id: 10000000,
+        },
+    },
+    {
+        divingFish: {
+            name: "OnlyDivingFish",
+            importToken: null,
+        },
+        inGame: {
+            name: null,
+            id: null,
+        },
+    },
+]
 </script>
 
+<template>
+    <div style="height: 10px;"></div>
+    <div class="user-cards-container">
+        <mdui-card variant="filled" v-for="user in users">
+            <div class="user-name">
+                <h2 class="primary-name">{{ (user.divingFish && user.divingFish.name) ?? (user.inGame && user.inGame.name) ?? "未知" }}</h2>
+                <div class="user-badges">
+                    <mdui-chip icon="videogame_asset" elevated :disabled="!user.divingFish || !user.divingFish.name">{{ user.divingFish.name ?? "未绑定水鱼" }}</mdui-chip>
+                    <mdui-chip icon="local_laundry_service" elevated :disabled="!user.inGame || !user.inGame.id">{{ user.inGame.name ?? user.inGame.id ?? "未绑定游戏" }}</mdui-chip>
+                </div>
+            </div>
+            <div class="user-actions">
+                <mdui-button-icon variant="standard" icon="settings"></mdui-button-icon>
+                <mdui-button-icon variant="standard" icon="update"></mdui-button-icon>
+                <mdui-button end-icon="arrow_forward">详情</mdui-button>
+                <!-- <mdui-button end-icon="settings">设置</mdui-button> -->
+            </div>
+        </mdui-card>
+    </div>
+</template>
+
 <style scoped>
+.user-cards-container {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
 mdui-card {
     width: 97.5%;
     /* height: 150px; */
@@ -28,8 +70,6 @@ mdui-card {
 
     display: flex;
     justify-content: space-between;
-
-    display: flex;
 }
 
 .user-name {
