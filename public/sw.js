22545d3b0fb7e7022e346bd4df6c5ea7cf4011cb
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-04-25 16:55:11 +0800

    fix

--- public/sw.js
+++ public/sw.js
@@ -109,7 +109,7 @@ self.addEventListener("fetch", async function (e) {
     if (urlOri.startsWith("http")) {
         if (urlParsed.path.endsWith("sw.v2.js") ||
           (urlParsed.path.endsWith(".json") && !urlParsed.path.endsWith("manifest.json")) ||
-          urlOri.includes("diving-fish.com/api") || urlParsed.host == "api.salt.realtvop.top"
+          urlOri.includes("diving-fish.com/api") || urlParsed.host == "api.salt.realtvop.top" || urlParsed.host == "salt_api_backup.realtvop.top"
         ) {
             return;
         } else if (urlOri.includes("diving-fish.com/covers")) {
