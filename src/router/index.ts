c4eaac2035fb1e07253fb8d51bbd16fabb5037c7
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-04-27 16:46:48 +0800

    feat: 添加设置页(todo)

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,14 @@ import IndexPage from '../pages/index.vue';
 import UserPage from '../pages/b50.vue';
 import AboutPage from '../pages/About.vue';
 import UsersPage from '../pages/users.vue';
+import SettingsPage from '../pages/settings.vue';
 
 const routes = [
   { path: '/', component: IndexPage },
   { path: '/users', component: UsersPage },
   { path: '/b50', component: UserPage },
   { path: '/b50/:id', component: UserPage, props: true },
+  { path: '/settings', component: SettingsPage },
   { path: '/about', component: AboutPage },
 ];
 
