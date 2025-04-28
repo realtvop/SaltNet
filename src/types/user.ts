605428419c133b11f69eee36f5d370f136b00cdf
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-04-28 21:23:43 +0800

    feat: 转换获取到的用户信息格式

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,5 +1,5 @@
 // src/types/user.ts
-import type { DivingFishB50 } from "@/divingfish/type";
+import type { DivingFishB50, DivingFishFullRecord } from "@/divingfish/type";
 
 export interface User {
     divingFish: {
@@ -13,5 +13,17 @@ export interface User {
     data: {
         rating?: number | null;
         b50?: DivingFishB50;
+        detailed?: DetailedData;
     };
+}
+
+type DetailedData = Record<number, DivingFishFullRecord>;
+
+export function convertDetailed(data: DivingFishFullRecord[]): DetailedData {
+    const result: DetailedData = {};
+
+    for (const item of data) 
+        result[Number(item.song_id)] = item;
+
+    return result;
 }
