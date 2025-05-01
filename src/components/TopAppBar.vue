47ee5e81478c866415f8edbb43e77c76deeb5892
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-05-01 08:20:54 +0800

    fix

--- src/components/TopAppBar.vue
+++ src/components/TopAppBar.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
 import { ref, computed } from 'vue';
-import RatingPlate from './RatingPlate.vue';
 
 const props = defineProps({
   playerInfo: {
