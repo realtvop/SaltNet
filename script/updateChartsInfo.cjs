8b36c293cacc4580e4228b42a711e9e41ceb82d8
飞机上的长电视 <realtvop@realtvop.top>
2025-07-08 16:09:46 +0000

    feat: 拟合定数等信息获取

--- script/updateChartsInfo.cjs
+++ script/updateChartsInfo.cjs
@@ -2,23 +2,27 @@
 const fs = require("fs");
 
 const DFMusicDataAPIURL = "https://www.diving-fish.com/api/maimaidxprober/music_data";
+const DFChartStatsAPIURL = "https://www.diving-fish.com/api/maimaidxprober/chart_stats";
 const LXMusicAliasAPIURL = "https://maimai.lxns.net/api/v0/maimai/alias/list";
 
-fetch(DFMusicDataAPIURL)
-    .then(r => r.json())
-    .then(data => {
-        fetch(LXMusicAliasAPIURL)
-            .then(r => r.json())
-            .then(res => {
-                const aliases = res.aliases;
+async function updateData() {
+    const musicData = await fetch(DFMusicDataAPIURL).then(r => r.json());
+    const chartStats = await fetch(DFChartStatsAPIURL).then(r => r.json());
+    const aliases = await fetch(LXMusicAliasAPIURL).then(r => r.json()).then(res => res.aliases);
 
-                for (const chart of data) {
-                    const id = Number(chart.id);
-                    const ids = 10000 < id < 100000 ? [id, id - 10000] : [id];
-                    const aliasList = aliases.find(a => ids.includes(a.song_id));
-                    if (aliasList) chart.aliases = aliasList.aliases;
-                }
+    for (const song of musicData) {
+        const id = Number(song.id);
+        const ids = 10000 < id < 100000 ? [id, id - 10000] : [id];
+        const aliasList = aliases.find(a => ids.includes(a.song_id));
+        if (aliasList) song.aliases = aliasList.aliases;
+        
+        const stats = chartStats.charts[song.id];
+        if (!stats) continue;
+        for (const chart of song.charts) 
+            chart.stats = stats[song.charts.indexOf(chart)] || {};
+    }
 
-                fs.writeFileSync("src/assets/music/charts.json", JSON.stringify(data), "utf8");
-            });
-    });
+    fs.writeFileSync("src/assets/music/charts.json", JSON.stringify(musicData), "utf8");
+}
+
+updateData()
