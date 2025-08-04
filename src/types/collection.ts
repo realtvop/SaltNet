84cdb788affc7159ad811d3e4c3e18dd89de0884
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-08-04 19:09:35 +0800

    feat: 支持收藏品查看的准备工作

--- src/types/collection.ts
+++ src/types/collection.ts
@@ -1,19 +1,36 @@
-enum CollectionType {
-    Icon = 'icon',   // 头像
-    Plate = 'plate', // 姓名框
-    Frame = 'frame', // 背景
-    Title = 'title', // 称号
-}
+export enum CollectionKind {
+    Title = 1, // 称号
+    Icon = 2,  // 头像
+    Plate = 3, // 姓名框
+    Frame = 4, // 背景
+};
+export enum TitleColor {
+    Bronze = 'Bronze',
+    Silver = 'Silver',
+    Gold = 'Gold',
+    Rainbow = 'Rainbow',
+};
 
 export interface Collection {
-    type: CollectionType;
+    type: CollectionKind;
     id: number;
     name: string;
     description: string;
-    genre: string;
 
-    status: CollectionStatus;
+    status?: CollectionStatus;
 }
+export interface Icon extends Collection {
+    genre: string;
+};
+export interface Plate extends Collection {
+    genre: string;
+};
+export interface Frame extends Collection {
+    genre: string;
+};
+export interface Title extends Collection {
+    color: TitleColor;
+};
 
 export interface CollectionStatus {
     owned: boolean;
