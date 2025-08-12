8704444dccabfd4e5421c5ca3ca2f1735f6a46dc
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-08-12 12:07:51 +0800

    fix: YouTube改名YT优化移动端显示

--- src/components/data/chart/getSearchUrls.ts
+++ src/components/data/chart/getSearchUrls.ts
@@ -9,7 +9,7 @@ export function getChartSearchUrls(chart: Chart): typeof VideoSearchUrls {
             url: template.url.replace(
                 "%s",
                 encodeURIComponent(
-                    `${chart.music.info.title} ${chart.music.info.type == ChartType.Deluxe ? "DX" : template.name === "YouTube" ? "スタンダード" : "标"} ${["BASIC", "ADVANDCED", "EXPERT", "MASTER", "Re:MASTER"][chart.info.grade]} ${chart.info.level}`
+                    `${chart.music.info.title} ${chart.music.info.type == ChartType.Deluxe ? "DX" : template.name === "YT" ? "スタンダード" : "标"} ${["BASIC", "ADVANDCED", "EXPERT", "MASTER", "Re:MASTER"][chart.info.grade]} ${chart.info.level}`
                 )
             ),
             icon: template.icon,
@@ -18,11 +18,8 @@ export function getChartSearchUrls(chart: Chart): typeof VideoSearchUrls {
     return urls;
 }
 const VideoSearchUrls = [
-    {
-        name: "YouTube",
-        url: `https://www.youtube.com/results?search_query=%s`,
-        icon: "smart_display",
-    },
+    // prettier-ignore
+    { name: "YT", url: `https://www.youtube.com/results?search_query=%s`, icon: "smart_display" },
     { name: "B站客户端", url: `bilibili://search?keyword=%s`, icon: "tv" },
     { name: "B站网页版", url: `https://search.bilibili.com/all?keyword=%s`, icon: "tv" },
 ];
