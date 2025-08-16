abc061f632a4adfae1971524810bb02794a31954
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-08-17 02:00:52 +0800

    fix: api fallback

--- src/components/integrations/SaltNet/index.ts
+++ src/components/integrations/SaltNet/index.ts
@@ -5,15 +5,27 @@ export function postAPI(endpoint: SaltAPIEndpoints, body: Object) {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(body),
-    }).catch(() =>
-        fetch(`${import.meta.env.VITE_API_FALLBACK_URL}/${endpoint}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(body),
+    })
+        .then(response => {
+            if (!response.ok)
+                return fetch(`${import.meta.env.VITE_API_FALLBACK_URL}/${endpoint}`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(body),
+                });
+            return response;
         })
-    );
+        .catch(() =>
+            fetch(`${import.meta.env.VITE_API_FALLBACK_URL}/${endpoint}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            })
+        );
 }
 
 export enum SaltAPIEndpoints {
