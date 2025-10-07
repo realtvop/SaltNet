971c30af48abfbc8afce4f585156d886c4bb69eb
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-10-07 13:13:55 +0800

    feat: 店铺搜索

--- src/pages/Shop.vue
+++ src/pages/Shop.vue
@@ -8,11 +8,16 @@
     const maxDistanceIndex = ref(2);
     const sliderRef = ref<HTMLElement | null>(null);
     const selectedShop = ref<Shop | null>(null);
+    const currentMode = ref<"all" | "nearby">("all");
+    const searchQuery = ref("");
 
     const filteredShops = computed(() => {
+        if (currentMode.value === "all") return nearShops.value;
         if (!nearShops.value) return null;
         return nearShops.value.filter(
-            shop => shop.distance! <= distanceSliderValues[maxDistanceIndex.value]
+            shop =>
+                shop.distance! <= distanceSliderValues[maxDistanceIndex.value] &&
+                shop.name.includes(searchQuery.value)
         );
     });
     onMounted(() => {
@@ -26,6 +31,14 @@
             };
         }
     });
+    function searchAllShops() {
+        if (currentMode.value !== "all") return;
+        fetch(`https://nearcade.phizone.cn/api/shops?q=${searchQuery.value}&limit=114514`)
+            .then(response => response.json())
+            .then(result => {
+                nearShops.value = result.shops;
+            });
+    }
     function getNearcades() {
         if (!navigator.geolocation) {
             snackbar({
@@ -44,6 +57,7 @@
                     .then(response => response.json())
                     .then(result => {
                         nearShops.value = result.shops;
+                        currentMode.value = "nearby";
                     });
             },
             error => {
@@ -59,33 +73,60 @@
     <mdui-list-item nonclickable>
         <div class="current-shop">
             <div>当前店铺: {{ selectedShop ? selectedShop.name : "无" }}</div>
-            <mdui-button-icon @click="getNearcades" icon="edit_location_alt"></mdui-button-icon>
+            <div>
+                <mdui-button-icon @click="currentMode = 'all'" icon="search"></mdui-button-icon>
+                <mdui-button-icon @click="getNearcades" icon="edit_location_alt"></mdui-button-icon>
+            </div>
         </div>
     </mdui-list-item>
 
-    <mdui-slider
-        ref="sliderRef"
-        :max="distanceSliderValues.length - 1"
-        @input.number="maxDistanceIndex = $event.target.value"
-        :value="maxDistanceIndex"
-        tickmarks
-    ></mdui-slider>
+    <mdui-tabs value="find" full-width>
+        <mdui-tab value="view">店铺</mdui-tab>
+        <mdui-tab-panel slot="panel" value="view">Panel 1</mdui-tab-panel>
 
-    <mdui-list>
-        <mdui-list-item
-            v-for="shop in filteredShops"
-            :key="shop.id"
-            @click="selectedShop = shop"
-            :active="shop.id === selectedShop?.id"
-        >
-            {{ shop.name }}
-            <div class="distance-badge" slot="end-icon">{{ shop.distance?.toFixed(2) }} km</div>
-            <span slot="description">
-                [{{ shop.address.general[shop.address.general.length - 1] }}]
-                {{ shop.address.detailed }}
-            </span>
-        </mdui-list-item>
-    </mdui-list>
+        <mdui-tab value="find">发现</mdui-tab>
+        <mdui-tab-panel slot="panel" value="find">
+            <mdui-text-field
+                label="搜索店铺"
+                icon="search"
+                @input="searchQuery = $event.target.value"
+                @keyup.enter="searchAllShops"
+                type="search"
+            >
+                <mdui-button-icon
+                    slot="end-icon"
+                    icon="arrow_forward"
+                    v-if="currentMode === 'all'"
+                ></mdui-button-icon>
+            </mdui-text-field>
+            <mdui-slider
+                ref="sliderRef"
+                :max="distanceSliderValues.length - 1"
+                @input.number="maxDistanceIndex = $event.target.value"
+                :value="maxDistanceIndex"
+                v-if="currentMode === 'nearby'"
+                tickmarks
+            ></mdui-slider>
+
+            <mdui-list>
+                <mdui-list-item
+                    v-for="shop in filteredShops"
+                    :key="shop.id"
+                    @click="selectedShop = shop"
+                    :active="shop.id === selectedShop?.id"
+                >
+                    {{ shop.name }}
+                    <div class="distance-badge" slot="end-icon" v-if="shop.distance">
+                        {{ shop.distance?.toFixed(2) }} km
+                    </div>
+                    <span slot="description">
+                        [{{ shop.address.general[shop.address.general.length - 1] }}]
+                        {{ shop.address.detailed }}
+                    </span>
+                </mdui-list-item>
+            </mdui-list>
+        </mdui-tab-panel>
+    </mdui-tabs>
 </template>
 
 <style scoped>
@@ -99,5 +140,12 @@
         border-radius: 1rem;
         background-color: rgb(var(--mdui-color-surface-container-highest));
         padding: 0.5rem;
+    }
+    mdui-slider {
+        margin-top: 0.5rem;
+    }
+    mdui-text-field {
+        margin-top: 0.5rem;
+        padding: 0 1rem;
     }
 </style>
